e47edf6734d312b9bd56ea28f6c02836c2a47317
Sanjay Andani <126229750+SanjayAndani@users.noreply.github.com>
2026-03-11 15:42:25 +0500

    update link

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -15,16 +15,16 @@ export default function Footer() {
                     Welcome to Engineers & Doctors Inn (E&D)! Established in 2012, our institute has been dedicated to empowering students to achieve their academic and career goals.
                   </p>
                   <div className="footer-social-icons">
-                    <a href="https://www.facebook.com/p/Engineers-Doctors-School-100063617213651/" target="_blank" className="facebook" title="Facebook" aria-label="Facebook">
+                    <a href="https://www.facebook.com/share/1ETfySUZLm/" target="_blank" className="facebook" title="Facebook" aria-label="Facebook">
                       <i className="fab fa-facebook-f"></i>
                     </a>
-                    <a href="https://www.instagram.com/" target="_blank" className="instagram" title="Instagram" aria-label="Instagram">
+                    {/* <a href="https://www.instagram.com/" target="_blank" className="instagram" title="Instagram" aria-label="Instagram">
                       <i className="fab fa-instagram"></i>
                     </a>
                     <a href="https://www.linkedin.com/" target="_blank" className="linkedin" title="LinkedIn" aria-label="LinkedIn">
                       <i className="fab fa-linkedin-in"></i>
-                    </a>
-                    <a href="https://www.youtube.com/" target="_blank" className="youtube" title="YouTube" aria-label="YouTube">
+                    </a> */}
+                    <a href="https://youtube.com/@engineersndoctorsinn?si=Wd3RA2fmImozemRq" target="_blank" className="youtube" title="YouTube" aria-label="YouTube">
                       <i className="fab fa-youtube"></i>
                     </a>
                   </div>
